882e79d8ebf5d5f6ec332a336d4eb1e9ea010aed
Ztaoxx <66443898+Ztaoxx@users.noreply.github.com>
2022-09-26 17:11:29 +0200

    Added Bet and Deposit functions.

--- Blackjack/main.py
+++ Blackjack/main.py
@@ -3,17 +3,18 @@ from deck import suits, values
 
 
 class Players:
-    def __init__(self, name, hand_list,points=0,bet=0):
+    def __init__(self, name, hand_list, points=0, bet=0 ,deposit=0):
         self.name = name
         self.hand_list = hand_list
         self.points = points
         self.bet = bet
+        self.deposit = deposit
+
 
     def draw_cards(self):
         self.hand_list.append(random.choice(deck))
         for card in self.hand_list:
             if self.hand_list.count(card) > 1:
-                print("CARD IS THE SAME!!")
                 self.hand_list.pop()
                 self.hand_list.append(random.choice(deck))
 
@@ -35,7 +36,6 @@ class Players:
 
 
 
-
 class Card:
     def __init__(self, suit, value, points=0):
         self.suit = suit
@@ -50,54 +50,116 @@ class Card:
         else:
             self.points = self.value
 
+
 class Game:
-    def __init__(self,game_over):
+    def __init__(self, game_over):
         self.game_over = game_over
 
-
-    def play(self):
+    def intro(self):
         print('BlackJack game will begin soon')
         print(f"Player's name is: {player1.name} and his rival is {computer.name}")
-        player1.draw_cards()
-        player1.draw_cards()
-        print(f'{player1.name} draws 2 cards:')
-        player1.show_cards()
-        player1.show_points()
-        print(f'{computer.name} draws 2 cards, one is hidden')
-        computer.draw_cards()
-        computer.show_cards()
-        computer.show_points()
+        player1.deposit = int(input("How much money do you want to start with?"))
+        start_game.play()
+
+    def game_continue(self):
+        if player1.deposit > 0:
+            choice = int(input("Do you want to continue? 1.Yes/2.No"))
+            if choice == 1:
+                self.deck_hand_refresh()
+                self.game_over = False
+                start_game.play()
+            else:
+                print("Thanks for playing")
+        else:
+            print("Your deposit is 0. Can't continue")
+    def deck_hand_refresh(self):
+        player1.hand_list = []
+        computer.hand_list = []
+
+    def play(self):
         while not start_game.game_over:
-            player_decision = int(input("Do you want 1.Hit or 2.Stand?"))
-            if player_decision == 1:
+            player1.bet = int(input("How much money do you want to bet?"))
+            if player1.bet > player1.deposit:
+                print(f"You can't bet this much, Your deposit is {player1.deposit}")
+            else:
+                print(f"{player1.name} starts with ${player1.deposit}")
+                print(f"{player1.name} bets ${player1.bet} on this game")
                 player1.draw_cards()
-                player1.show_card()
+                player1.draw_cards()
+                print(f'{player1.name} draws 2 cards:')
+                player1.show_cards()
                 player1.show_points()
-                if player1.points > 21:
-                    print(f'{computer.name} WINS! You overdrew!')
-                    start_game.game_over = True
-                elif player1.points == 21:
-                    print(f'{player1.name} has won the BLACKJACK!')
-            elif player_decision == 2:
-                while computer.points <= 17:
-                    computer.draw_cards()
-                    computer.show_card()
-                    computer.show_points()
-                    if computer.points >= 17:
-                        print(f'The Crupier got 17 or more points worth of cards.')
-                        print('Calculating points between player and Crupier!')
-                        print(f'{player1.name} points: {player1.points}\t{computer.name} points: {computer.points}')
-                        if player1.points > computer.points or computer.points > 21:
-                            print(f'{player1.name} has won the game!')
-                            start_game.game_over = True
-                        else:
-                            print(f'{computer.name} WINS!')
-                            start_game.game_over = True
+                print(f'{computer.name} draws 2 cards, one is hidden')
+                computer.draw_cards()
+                computer.show_cards()
+                computer.show_points()
+
+                player_decision = int(input("Do you want 1.Hit, 2.Stand, 3.Double, 4.Split"))
+                if player_decision == 1:
+                    player1.draw_cards()
+                    player1.show_card()
+                    player1.show_points()
+                    win_condition()
+                elif player_decision == 2:
+                    while computer.points <= 17:
+                        computer.draw_cards()
+                        computer.show_card()
+                        computer.show_points()
+                        win_condition()
+
+                elif player_decision == 3 and len(player1.hand_list) == 2:
+                    player1.bet *= 2
+                    player1.draw_cards()
+                    player1.show_card()
+                    player1.show_points()
+                    win_condition()
+                    while computer.points <= 17:
+                        computer.draw_cards()
+                        computer.show_card()
+                        computer.show_points()
+                        win_condition()
+
+                elif player_decision == 3 and len(player1.hand_list) > 2:
+                    print("You can't double. Already have more than 2 cards!")
+
+
+def win_condition():
+    if player1.points > 21:
+        print(f'{computer.name} WINS! You overdrew!')
+        player1.deposit -= player1.bet
+        print(f'You lose {player1.bet} and your deposit is {player1.deposit}')
+        start_game.game_over = True
+        start_game.game_continue()
+    elif player1.points == 21:
+        print(f'{player1.name} has won the BLACKJACK!')
+        player1.bet *= 2
+        player1.deposit += player1.bet
+        print(f'You have won {player1.bet} and your deposit is {player1.deposit}')
+        start_game.game_over = True
+        start_game.game_continue()
+    elif computer.points >= 17:
+        print(f'The Crupier got 17 or more points worth of cards.')
+        print('Calculating points between player and Crupier!')
+        print(f'{player1.name} points: {player1.points}\t{computer.name} points: {computer.points}')
+        if player1.points > computer.points or computer.points > 21:
+            print(f'{player1.name} has won the game!')
+            player1.bet *= 2
+            player1.deposit += player1.bet
+            print(f'You have won {player1.bet} and your deposit is {player1.deposit}')
+            start_game.game_over = True
+            start_game.game_continue()
+        else:
+            print(f'{computer.name} WINS!')
+            player1.deposit -= player1.bet
+            print(f'You lose {player1.bet} and your deposit is {player1.deposit}')
+            start_game.game_over = True
+            start_game.game_continue()
+
 if __name__ == "__main__":
-    player1 = Players('Dominik', [])
+    player1 = Players('Player', [])
     computer = Players('Computer', [])
     deck = [Card(suit, value) for suit in suits for value in values]
     for cards in deck:
         cards.give_points()
     start_game = Game(False)
-    start_game.play()
+    start_game.intro()
